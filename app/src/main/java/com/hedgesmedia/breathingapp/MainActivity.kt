92b3a44073d1a4f0e9f8b7641041a43ed9abed84
jzenisek <zenisekjordan@gmail.com>
2017-10-21 16:39:46 -0400

    Updated Button two.

--- app/src/main/java/com/hedgesmedia/breathingapp/MainActivity.kt
+++ app/src/main/java/com/hedgesmedia/breathingapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.hedgesmedia.breathingapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun whatever(view: View){
+        val testIntent = Intent(this, Gif2::class.java)
+        startActivity(testIntent)
     }
 }
